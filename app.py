76c057370436b8f604fc5fee0c81de880238ac6a
Letícia Tambelli <leticiatambelli2@gmail.com>
2025-06-20 12:36:45 -0300

    adicao instancias teste

--- app.py
+++ app.py
@@ -16,8 +16,31 @@ class Pessoa:
             return f'Olá, sou {self.nome}! Trabalho como {self.profissao}.'
         else:
             return f'Olá, eu sou {self.nome}'
-        
 
+
+# Criando 3 instâncias da classe Pessoa
 pessoa1 = Pessoa(nome='Alice', idade=25, profissao='Engenheira')
 pessoa2 = Pessoa(nome='Luiza', idade=30, profissao='Desenvolvedor')
 pessoa3 = Pessoa(nome='Jaque', idade=22)
+
+# Imprimindo informações iniciais
+print("Informações Iniciais:")
+print(pessoa1)
+print(pessoa2)
+print(pessoa3)
+print()
+
+# Utilizando o método de instância aniversario para aumentar a idade de uma pessoa
+pessoa1.aniversario()
+pessoa3.aniversario()
+
+# Imprimindo informações após aniversário
+print("Informações após aniversário:")
+print(pessoa1)
+print(pessoa3)
+print()
+
+# Utilizando o método de classe saudacao para exibir mensagens personalizadas
+print(pessoa1.mensagem)
+print(pessoa2.mensagem)
+print(pessoa3.mensagem)
