56e7314e842ec905a12bb5d7028b5a22167ec2e5
Letícia Tambelli <leticiatambelli2@gmail.com>
2025-06-20 12:32:12 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+class Livro:
+    def __init__(self, nome='', idade=0, profissao=''):
+        self.nome = nome
+        self.idade = idade
+        self.profissao = profissao
+
+    def __str__(self):
+        return f'{self.nome}, {self.idade} anos, {self.profissao}'
+    
+    def aniversario(self):
+        self.idade += 1
+
+    @property
+    def mensagem(self):
+        if self.profissao:
+            return f'Olá, sou {self.nome}! Trabalho como {self.profissao}.'
+        else:
+            return f'Olá, eu sou {self.nome}'
