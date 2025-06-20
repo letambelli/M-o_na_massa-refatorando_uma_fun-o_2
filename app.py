1387cafc847616d0e93f331204e638ae80e3f35b
Letícia Tambelli <leticiatambelli2@gmail.com>
2025-06-20 12:34:25 -0300

    correcao nome classe

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-class Livro:
+class Pessoa:
     def __init__(self, nome='', idade=0, profissao=''):
         self.nome = nome
         self.idade = idade
@@ -16,3 +16,8 @@ class Livro:
             return f'Olá, sou {self.nome}! Trabalho como {self.profissao}.'
         else:
             return f'Olá, eu sou {self.nome}'
+        
+
+pessoa1 = Pessoa(nome='Alice', idade=25, profissao='Engenheira')
+pessoa2 = Pessoa(nome='Luiza', idade=30, profissao='Desenvolvedor')
+pessoa3 = Pessoa(nome='Jaque', idade=22)
